9a2c196800cd90e5474c0dcf610cebda3f2c45f7
Arnold P Mavhezha <arnoemavhezha@gmail.com>
2023-04-19 15:01:19 +0000

    Fetch data from endpoint using axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./styles.css";
+import DataFetching from './components/DataFetching'
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1 className="header">Xavi Hive!</h1>
+      
+      <DataFetching />
     </div>
   );
 }
